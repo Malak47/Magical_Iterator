e3a51d461e6b2e278c669b06c2ddb9e41486f003
malaklinux <malak.qeedan@hotmail.com>
2023-06-04 16:48:30 +0300

    Implementing MagicalContainer

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -4,20 +4,21 @@
 
 
 #include "MagicalContainer.hpp"
+#include "cmath"
 
 using namespace ariel;
 
-MagicalContainer::MagicalContainer() : vec(0) {
+MagicalContainer::MagicalContainer() : vecElements(), vecPrime() {
 }
 
 void MagicalContainer::addElement(int element) {
-    vec.push_back(element);
+    vecElements.push_back(element);
 }
 
 bool MagicalContainer::removeElement(int element) {
-    for (auto it = vec.begin(); it != vec.end(); ++it) {
+    for (auto it = vecElements.begin(); it != vecElements.end(); ++it) {
         if (*it == element) {
-            vec.erase(it);
+            vecElements.erase(it);
             return true; // Element was found and removed
         }
     }
@@ -26,9 +27,40 @@ bool MagicalContainer::removeElement(int element) {
 
 
 size_t MagicalContainer::size() const {
-    return vec.size();
+    return vecElements.size();
 }
 
 vector<int> MagicalContainer::getElements() {
-    return vec;
+    return vecElements;
+}
+
+void MagicalContainer::addAndSortElement(int element, vector<int> &vec) {
+    auto it = vec.begin();
+
+    vec.insert(it, element);
+}
+
+bool MagicalContainer::isPrime(int number) {
+    if (number == 2) {
+        return true;
+    }
+    if (number <= 1 || number % 2 == 0) {
+        return false;
+    }
+    int i = 3;
+    while (i <= sqrt(number)) {
+        if (number % i == 0) {
+            return false;
+        }
+        i += 2;
+    }
+    return true;
+}
+
+int MagicalContainer::searchElementAt(int element, const vector<int> &vec) {
+    auto it = std::find(vec.begin(), vec.end(), element);
+    if (it != vec.end()) {
+        return std::distance(vec.begin(), it);
+    }
+    return -1;
 }
--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -11,20 +11,26 @@
 
 using namespace std;
 namespace ariel {
+
     class MagicalContainer {
     private:
-        vector<int> vec;  // elements in the MagicalContainer
-
+        vector<int> vecElements;
+        vector<int> vecPrime;
     public:
         MagicalContainer();
 
         void addElement(int element);
-
         bool removeElement(int element);
-
         size_t size() const;
-
         vector<int> getElements();
+
+        int searchElementAt(int element, const vector<int>& vec);       //O(N)
+        void addAndSortElement(int element, vector<int> &vec);
+        static bool isPrime(int number);
+
+        //****************************************************************************************
+        //********** A S C E N D I N G   I T E R A T O R *****************************************
+        //****************************************************************************************
 
         class AscendingIterator {
         private:
@@ -32,40 +38,32 @@ namespace ariel {
             int index;
         public:
             AscendingIterator();
-
             AscendingIterator(const AscendingIterator &other);
-
             AscendingIterator(MagicalContainer &container);
-
             ~AscendingIterator();
-
-            AscendingIterator(AscendingIterator &&) noexcept = delete; // Move Constructor.
+            AscendingIterator(AscendingIterator &&) noexcept = delete;
             AscendingIterator &operator=(AscendingIterator &&) noexcept = delete;
-
             AscendingIterator &operator=(const AscendingIterator &other);
 
             bool operator==(const AscendingIterator &other) const;
-
             bool operator!=(const AscendingIterator &other) const;
-
             bool operator>(const AscendingIterator &other) const;
-
             bool operator<(const AscendingIterator &other) const;
-
             int operator*() const;
 
             AscendingIterator &operator++();
-
             AscendingIterator operator++(int);
 
             AscendingIterator begin();
-
             AscendingIterator end();
 
             MagicalContainer getContainer() const;
-
             int getIndex() const;
         };
+
+        //****************************************************************************************
+        //********** S I D E - C R O S S   I T E R A T O R ***************************************
+        //****************************************************************************************
 
         class SideCrossIterator {
         private:
@@ -73,85 +71,63 @@ namespace ariel {
             int frontIndex, backIndex;
         public:
             SideCrossIterator();
-
             SideCrossIterator(const SideCrossIterator &other);
-
             SideCrossIterator(MagicalContainer &container);
-
             ~SideCrossIterator();
-
             SideCrossIterator(SideCrossIterator &&) noexcept = delete; // Move Constructor.
             SideCrossIterator &operator=(SideCrossIterator &&) noexcept = delete;
-
             SideCrossIterator &operator=(const SideCrossIterator &other);
 
             bool operator==(const SideCrossIterator &other) const;
-
             bool operator!=(const SideCrossIterator &other) const;
-
             bool operator>(const SideCrossIterator &other) const;
-
             bool operator<(const SideCrossIterator &other) const;
-
             int operator*() const;
 
             SideCrossIterator &operator++();
-
             SideCrossIterator operator++(int);
 
             SideCrossIterator begin();
-
             SideCrossIterator end();
 
             MagicalContainer getContainer() const;
-
             int getFrontIndex() const;
-
             int getBackIndex() const;
 
         };
+
+        //****************************************************************************************
+        //********** P R I M E   I T E R A T O R *************************************************
+        //****************************************************************************************
 
         class PrimeIterator {
         private:
             MagicalContainer *container;
             int index;
-
             bool isPrime(int num);
 
         public:
             PrimeIterator();
-
             PrimeIterator(const PrimeIterator &other);
-
             PrimeIterator(MagicalContainer &container);
-
             ~PrimeIterator();
-
             PrimeIterator(PrimeIterator &&) noexcept = delete; // Move Constructor.
             PrimeIterator &operator=(PrimeIterator &&) noexcept = delete;
-
             PrimeIterator &operator=(const PrimeIterator &other);
 
             bool operator==(const PrimeIterator &other) const;
-
             bool operator!=(const PrimeIterator &other) const;
-
             bool operator>(const PrimeIterator &other) const;
-
             bool operator<(const PrimeIterator &other) const;
-
             int operator*() const;
 
             PrimeIterator &operator++();
-
             PrimeIterator operator++(int);
 
             PrimeIterator begin();
-
             PrimeIterator end();
 
             MagicalContainer getContainer() const;
-
             int getIndex() const;
         };
     };
